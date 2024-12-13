30e3ed9b6a1720fd90b18399c545a24a4ab9ac5a
bulutluoz <abbulutluoz@gmail.com>
2024-12-13 12:48:24 +0100

    Ders13 sonu

---
+++ src/test/java/pages/TestotomasyonuPage.java
@@ -0,0 +1,25 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import utilities.Driver;
+
+public class TestotomasyonuPage {
+
+    @FindBy(id = "global-search")
+    public WebElement aramaKutusu;
+
+    @FindBy(className = "product-count-text")
+    public WebElement aramaSonucuElementi;
+
+
+    @FindBy(xpath = "(//*[@class='prod-img'])[1]")
+    public WebElement ilkUrunElementi;
+
+    @FindBy(xpath = "//div[@class=' heading-sm mb-4']")
+    public WebElement ilkUrunSayfasindakiIsimElementi;
+
+
+
+}
