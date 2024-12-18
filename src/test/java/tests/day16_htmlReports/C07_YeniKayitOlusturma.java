f8b063ef7c19700ee366db35f53c100927078377
bulutluoz <abbulutluoz@gmail.com>
2024-12-18 11:40:39 +0100

    Ders16 sonu

---
+++ src/test/java/tests/day16_htmlReports/C07_YeniKayitOlusturma.java
@@ -0,0 +1,21 @@
+package tests.day16_htmlReports;
+
+import org.testng.annotations.Test;
+
+public class C07_YeniKayitOlusturma {
+
+    // Farkli datalarla tabloya 5 kayit ekleyip, asagidaki testi yapin.
+    //	When kullanici https://editor.datatables.net/ adresine gider
+    //	Then new butonuna basar
+    //	And tum bilgileri girer
+    //	And Create tusuna basar
+    //	When kullanici ilk isim ile arama yapar
+    //	Then isim bolumunde isminin oldugunu dogrular
+
+
+    @Test
+    public void kayitTesti(String firstName, String lastName, String position,
+                           String office, String extension, String startDate, int salary){
+
+    }
+}
